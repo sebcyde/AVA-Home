a783b0b2a47a5ca73b3cb36b2fd511a87df2f7e6
Sebastian <sebcyde@hotmail.com>
2023-06-20 00:00:31 +0100

    minor abstractions

--- avaSettings.cs
+++ avaSettings.cs
@@ -25,8 +25,6 @@ public class avaSettings
         OpenAIAPI api = new OpenAIAPI(new APIAuthentication(API_KEY));
         var chat = api.Chat.CreateConversation();
         chat.AppendSystemMessage("You are a female AI personal assistant named 'AVA' that helps the user, 'Sebastian' with various tasks on his computer, similar to Jarvis from Iron Man. Speak like a cool anime girl.");
-        
-        
         chat.AppendUserInput("Greet me and ask me what I'd like to do. Also speak like a cool anime girl from now on");
 
         Console.Write("AVA: ");
@@ -38,57 +36,51 @@ public class avaSettings
         while (isRunning)
         {
 
-            Console.WriteLine(" ");
-            Console.WriteLine(" ");
-            Console.Write("You: ");
+            introduceUser();
+
             string userInput = Console.ReadLine();
 
             // Check if the user wants to stop AVA
-            if (userInput.ToLower() == "thats all")
+            if (isCancellingAVA(userInput))
             {
-                isStopped = true;
-                isRunning = false;
+                beginAVAShutdown();
                 chat.AppendUserInput("thats all, thanks for your help");
             }
 
             // Open this project in Visual Studio
-            if (userInput.ToLower() == "open your settings" || userInput.ToLower() == "show me your settings")
+            if (isOpeningSettings(userInput))
             {
-                isStopped = true;
-                isRunning = false;
+                beginAVAShutdown();
                 chat.AppendUserInput("pretend you're about to open you settings for me to edit");
                 openAVASettings();
             }
 
             // Spin up a custom React App with optional Typescript
-            if (userInput.ToLower() == "create a new react app" || userInput.ToLower() == "create react app")
+            if (isCreatingReactApp(userInput))
             {
-                isStopped = true;
-                isRunning = false;
+                beginAVAShutdown();
                 chat.AppendUserInput("Wish me luck on the new React app I'm about to make.");
                 ReactFunctions.CreateReactApp(); ;
             }
 
             // Spin up a custom C# App
-            if (userInput.ToLower() == "create a new c# app" || userInput.ToLower() == "create c# app"){
-                isStopped = true;
-                isRunning = false;
+            if (isCreatingCApp(userInput))
+            {
+                beginAVAShutdown();
                 chat.AppendUserInput("Wish me luck on the new C# app I'm about to make.");
                 CSharpFunctions.CreateNewApp();
             }
 
             // Start entertainment mode without any codeing stuff
-            if (userInput.ToLower() == "start entertainment mode")
+            if (isStartingEntertainmentMode(userInput))
             {
-                isStopped = true;
-                isRunning = false;
+                beginAVAShutdown();
                 chat.AppendUserInput("I'm going to listen to music for a bit, I'll talk to you later.");
-                //BashCommands.ExecuteBashCommand("printf \"\\033[3;7;10t\"");
                 EntertainmentModeClass.EntertainmentMode();
             }
 
-            Console.WriteLine(" ");
-            Console.Write("AVA: ");
+            introduceAVA();
+
             await foreach (var response in chat.StreamResponseEnumerableFromChatbotAsync())
             {
                 Console.Write(response);
@@ -104,18 +96,34 @@ public class avaSettings
 
     }
 
-    public static void StopAVA()
+    private static void StopAVA()
     {
         Console.WriteLine(" ");
         FreeConsole();
-        // Add any additional cleanup or termination logic here
     }
 
+    private static void beginAVAShutdown()
+    {
+        isStopped = true;
+        isRunning = false;
+    }
 
-    public static void openAVASettings()
+    private static void introduceAVA()
+    {
+        Console.WriteLine(" ");
+        Console.Write("AVA: ");
+    }
+
+    private static void introduceUser()
+    {
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.Write("You: ");
+    }
+
+    private static void openAVASettings()
     {
 
-        // Run 'npm install'
         ProcessStartInfo openSettingsProfileInfo = new ProcessStartInfo
         {
             FileName = @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe",
@@ -133,10 +141,93 @@ public class avaSettings
         };
 
         openSettingsProcess.Start();
-        //FreeConsole();
-
 
     }
+
+    private static bool isOpeningSettings(string input)
+    {
+        switch (input.ToLower())
+        {
+            case ("open your settings"):
+                return true;
+                break;
+            case ("show me your settings"):
+                return true;
+                break;
+            case ("open your code"):
+                return true;
+                break;
+            case ("show me your code"):
+                return true;
+                break;
+            default:
+                return false;
+        }
+    }
+
+    private static bool isCreatingReactApp(string input)
+    {
+
+        switch (input.ToLower())
+        {
+            case ("create a new react app"):
+                return true;
+                break;
+            case ("create react app"):
+                return true;
+                break;
+            default:
+                return false;
+        }
+
+    }
+
+    private static bool isCreatingCApp(string input)
+    {
+
+        switch (input.ToLower())
+        {
+            case ("create a new c# app"):
+                return true;
+                break;
+            case ("create c# app"):
+                return true;
+                break;
+            default:
+                return false;
+        }
+
+    }
+
+    private static bool isStartingEntertainmentMode(string input)
+    {
+
+        switch (input.ToLower())
+        {
+            case ("start entertainment mode"):
+                return true;
+                break;
+            case ("entertainment mode"):
+                return true;
+                break;
+            default:
+                return false;
+        }
+
+    }
+
+    private static bool isCancellingAVA(string input)
+    {
+        if (input.ToLower() == "thats all" || input.ToLower() == "c")
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+
 }
     
 
